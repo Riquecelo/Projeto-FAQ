7d278e8064b61aa281705bae5999615297d98c1d
Marcelo Santos <riquecelox462m@gmail.com>
2021-11-06 23:43:49 -0300

    adição de script

--- faq.js
+++ faq.js
@@ -1,4 +1,28 @@
-const produto = {
-    nome: 'Tenis Nike',
-    valor: 10000
-}
+//buscar items da tela
+const itemsPerguntasRespostas = document.querySelectorAll('.item');
+
+//entender que o item foi clicado
+//  forEach[para cada] elemento dentro da constante que tenha 'item' execute a função 
+itemsPerguntasRespostas.forEach(function(item){
+    //escuta o evento que está acontecedo com o item e executa a função
+    item.addEventListener('click', function(){
+    //verificar se a pergunta clicada foi ativa
+        const estaAtivo = item.classList.contains('ativo')
+
+    // se a pergunta não está ativa
+        if(!estaAtivo){
+             //abrir a resposta atual, mas eu preciso fechar todas
+             itemsPerguntasRespostas.forEach(function(item){
+                 item.classList.remove('ativo')
+             })
+
+            //abrir resposta atual 
+            item.classList.add('ativo')
+        }
+       
+
+    // se a pergunta estiver ativa
+        //desativar[remover a classe ativa]
+    })
+})
+
